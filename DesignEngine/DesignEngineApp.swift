05eda622dd03058de210a1239d9b304c05dcb0f8
Juan Diego Ocampo <gummy_pond0v@icloud.com>
2023-03-09 09:48:21 -0500

    Initial Commit

---
+++ DesignEngine/DesignEngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignEngineApp.swift
+//  DesignEngine
+//
+//  Created by Juan Diego Ocampo on 2023-03-09.
+//
+
+import SwiftUI
+
+@main
+struct DesignEngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
