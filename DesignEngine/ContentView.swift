9b4d526aa01badd98742334210c0feca6f2d1c04
Juan Diego Ocampo <gummy_pond0v@icloud.com>
2023-04-19 08:34:15 -0500

    MB-726: Extracted all user interface components and models to the prototype swift package

--- DesignEngine/ContentView.swift
+++ DesignEngine/ContentView.swift
@@ -10,53 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @ObservedObject private var viewModel = ContentViewModel()
-    
     var body: some View {
-        
-        NavigationView {
-            
-            VStack(alignment: .leading, spacing: .zero) {
-                
-                VStack(alignment: .leading, spacing: .zero) {
-                    ForEach(viewModel.headerRows, id: \.id) { row in
-                        ForEach(row.fields, id: \.id) { field in
-                            Text(field.id)
-                        }
-                    }
-                }
-                
-                ScrollView {
-                    ForEach(viewModel.detailRows, id: \.id) { row in
-                        ForEach(row.fields, id: \.id) { field in
-                            if field.contents.type == "TextInput" {
-                                ITTextInputView(withRow: row, andTintColor: .brandPrimary)
-                            }
-                        }
-                    }
-                }
-                
-                Spacer()
-                
-                VStack(alignment: .leading, spacing: .zero) {
-                    ForEach(viewModel.footerRows, id: \.id) { row in
-                        ForEach(row.fields, id: \.id) { field in
-                            if field.contents.type == "Button" {
-                                ITButtonView(withRow: row, andTintColor: .brandPrimary)
-                            }
-                        }
-                    }
-                }
-                
-            }
-            
-            .navigationTitle(viewModel.title)
-            .onAppear {
-                viewModel.updateUI()
-            }
-        }
-        
+        ITScreenView(
+            withViewElement: Bundle.main.decode("passenger_on_demand.json"),
+            andTintColor: .brandPrimary
+        )
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
